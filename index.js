2f3aff991a46b6256d5dfb7aac761adcf4662fa6
renatoTheoJr <renatotheojunior@gmail.com>
2022-09-22 16:42:50 -0300

    Challenge

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+app.get('/api/whoami', function (request, response) {
+  const {headers} = request;
+  console.log(headers['user-agent']);
+  response.json({ipaddress: headers.host, language: headers['accept-language'],software: headers['user-agent']});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
